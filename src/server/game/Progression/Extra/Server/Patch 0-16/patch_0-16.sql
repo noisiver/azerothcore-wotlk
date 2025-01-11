8cf05d722a591de74098e7e8b6cc0b76f840b6d4
Revision <tkn963@gmail.com>
2025-01-11 23:54:41 +0100

    Naxxramas
    Modified the map to move the old entrance to Naxxramas back up and added triggers to teleport to Naxxramas. It's still very much a work in progress and shouldn't be used at this stage.

--- src/server/game/Progression/Extra/Server/Patch 0-16/patch_0-16.sql
+++ src/server/game/Progression/Extra/Server/Patch 0-16/patch_0-16.sql
@@ -7,3 +7,15 @@ INSERT INTO `mapdifficulty_dbc` (`ID`, `MapID`, `Difficulty`, `RaidDuration`, `M
 (24, 229, 0, 0, 10, NULL),
 (46, 533, 0, 604800, 40, 'RAID_DIFFICULTY_40PLAYER'),
 (128, 533, 1, 604800, 40, 'RAID_DIFFICULTY_40PLAYER');
+
+DELETE FROM `gameobject` WHERE `id`=181476;
+INSERT INTO `gameobject` (`id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `animprogress`, `state`) VALUES
+(181476, 0, 3132.72, -3731.23, 135, 1.11934, -0, -0, -0.530907, -0.84743, 0, 1);
+
+DELETE FROM `areatrigger` WHERE `entry`=4055;
+INSERT INTO `areatrigger` (`entry`, `map`, `x`, `y`, `z`, `radius`, `length`, `width`, `height`, `orientation`) VALUES
+(4055, 0, 3132.72, -3731.23, 138.88182, 5, 0, 0, 0, 0);
+
+DELETE FROM `areatrigger_teleport` WHERE `ID`=4055;
+INSERT INTO `areatrigger_teleport` (`ID`, `Name`, `target_map`, `target_position_x`, `target_position_y`, `target_position_z`, `target_orientation`) VALUES
+(4055, 'Naxxramas', 533, 3005.59, -3434.49, 304.196, 0);
--- src/server/game/Progression/Extra/Server/Patch 17-21/patch_17-21.sql
+++ src/server/game/Progression/Extra/Server/Patch 17-21/patch_17-21.sql
@@ -1,2 +1,5 @@
 DELETE FROM `map_dbc` WHERE `ID`=533;
 DELETE FROM `mapdifficulty_dbc` WHERE `MapID` IN (229, 533);
+DELETE FROM `gameobject` WHERE `id`=181476;
+DELETE FROM `areatrigger` WHERE `entry`=4055;
+DELETE FROM `areatrigger_teleport` WHERE `ID`=4055;
