34869fa082baca00f6347c550ccf137951b6fc8c
Revision <tkn963@gmail.com>
2025-01-12 20:20:20 +0100

    Naxxramas
    Merged the current progress for Naxxramas into the regular data.
    Replaced additions to the *_dbc tables with actual DBC changes. It just makes sense.

--- src/server/game/Progression/Extra/Server/Patch 0-16/patch_0-16.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM `mapdifficulty_dbc` WHERE `MapID`=229;
-INSERT INTO `mapdifficulty_dbc` (`ID`, `MapID`, `Difficulty`, `RaidDuration`, `MaxPlayers`, `Difficultystring`) VALUES
-(24, 229, 0, 0, 10, NULL);
